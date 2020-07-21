61838275dfd160b820022870857ea9cd9e399c8c
Anya Brun <tsbrun@icloud.com>
2020-07-21 20:14:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,13 @@
 def hello_t(names)
-  i = 0
+  if block_given?
+    i = 0
 
-  while i < names.length
-    yield names[i]
-    i = i + 1
+    while i < names.length
+      yield names[i]
+      i = i + 1
+    end
+  else
+    puts "Hey! No block was given!"
   end
   
   names
